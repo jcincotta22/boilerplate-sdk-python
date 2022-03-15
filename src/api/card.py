c9a42860ab6cbed999d66fdf809c226282e68566
Jeff Cincotta <jcincotta22@gmail.com>
2022-03-15 10:50:36 -0400

    move to one file

--- src/api/card.py
+++
@@ -1,6 +0,0 @@
-class Card(object):
-    def __init__(self, image: str, value: str, suit: str, code: str):
-        self.image = image
-        self.value = value
-        self.suit = suit
-        self.code = code
